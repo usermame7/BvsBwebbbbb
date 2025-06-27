438e19ff5df256741249d9c70efe54b3af86aad4
usermame7 <154531526+usermame7@users.noreply.github.com>
2025-06-27 13:09:41 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,309 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Bulls vs Bears</title>
+        <meta name="description" content="Binary Options Trading Platform" />
+      </Head>
+      <div dangerouslySetInnerHTML={{ __html: `<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Tue Dec 05 2023 19:26:34 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="656751a4fe95d82133d70f5c" data-wf-site="656751a4fe95d82133d70f59">
+<head>
+  <meta charset="utf-8">
+  <title>upvsdown</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/upvsdown.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Oswald:200,300,400,500,600,700"]  }});</script>
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+<body class="body">
+  <div class="header">
+    <div class="menu_left">
+      <a href="#" class="button_chat w-button"></a>
+      <div class="profil"><img src="images/avatar_man.png" loading="lazy" alt="" class="profil_pic"></div>
+      <div class="menu">
+        <div class="ham"></div>
+        <div class="ham"></div>
+        <div class="ham"></div>
+      </div>
+    </div>
+    <div class="header_top"></div>
+    <div class="header_block_menu">
+      <div class="menu_center">
+        <a href="link.html" class="button_menu w-button">AFFILIATES</a>
+        <a href="#" class="button_connect w-button">CONNECT WALLET</a>
+        <div class="glow_connect"></div>
+        <a href="dahsboard.html" class="button_menu w-button">WINNERS</a>
+      </div><img src="images/connect_bar.svg" loading="lazy" alt="" class="connect_bar">
+    </div>
+  </div>
+  <div class="section_main">
+    <div class="left_panel">
+      <div class="block_players">
+        <div class="block_info">
+          <div class="title_pool_green">UP POOL TREASURY</div>
+          <div class="line_green"></div>
+          <div class="pool_info">
+            <div class="pool_amount">
+              <div class="chain_number">100</div>
+            </div>
+            <div class="pool_amount">
+              <div class="players"><img src="images/people_green.svg" loading="lazy" alt="" class="player_icon"></div>
+              <div class="text_player_green">15</div>
+            </div>
+          </div>
+        </div>
+        <div class="block_player">
+          <div class="raw_players">
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+          </div>
+          <div class="raw_players">
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+          </div>
+          <div class="raw_players">
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_green">
+              <div class="player_pic_green">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="player_bet">25 ETH</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="push_green"><img src="images/arrow_up.svg" loading="lazy" alt="" class="arrow_green"></div>
+    </div>
+    <div class="center_panel">
+      <div class="event_details">
+        <div class="event_data">
+          <div class="event_left">
+            <div class="small_title">YOUR INVESTMENT</div>
+            <div class="data_title">10.0</div>
+          </div>
+          <div class="event_left">
+            <div class="small_title">POTENTIAL RETURN</div>
+            <div class="data_title">14.0</div>
+          </div>
+        </div>
+        <div class="data_info">
+          <div class="middle">
+            <div class="up_pool">UP POOL PAYOUT</div>
+            <div class="up_pool_number">152%</div>
+          </div>
+          <div class="timer">
+            <div class="timer_in">
+              <div class="time">08</div>
+              <div class="sec">SEC</div>
+            </div>
+          </div>
+          <div class="middle">
+            <div class="down_pool">DOWN POOL PAYOUT</div>
+            <div class="down_pool_number">225%</div>
+          </div>
+        </div>
+        <div class="event_data">
+          <div class="event_right">
+            <div class="small_title">YOUR INVESTMENT</div>
+            <div class="data_title">10.0</div>
+          </div>
+          <div class="event_right">
+            <div class="small_title">POTENTIAL RETURN</div>
+            <div class="data_title">14.0</div>
+          </div>
+        </div>
+      </div>
+      <div class="block_chart">
+        <div class="green"></div>
+        <div class="chart_separator"></div>
+        <div class="red"></div>
+        <a href="weekly-jackpot.html" class="button_jackpot w-button">JACKPOT</a>
+      </div>
+      <div class="block_bet">
+        <a href="#" class="bet_button w-button">5</a>
+        <a href="#" class="bet_button w-button">10</a>
+        <a href="#" class="bet_button w-button">15</a>
+        <a href="#" class="bet_button w-button">25</a>
+        <a href="#" class="bet_button w-button">50</a>
+        <a href="#" class="bet_button w-button">75</a>
+        <a href="#" class="bet_button w-button">100</a>
+      </div>
+    </div>
+    <div class="right_panel">
+      <div class="block_players">
+        <div class="block_info">
+          <div class="title_pool_red">DOWN POOL TREASURY</div>
+          <div class="line_red"></div>
+          <div class="pool_info">
+            <div class="pool_amount">
+              <div class="players"><img src="images/people_red.svg" loading="lazy" alt=""></div>
+              <div class="text_player_red">15</div>
+            </div>
+            <div class="pool_amount">
+              <div class="chain_number">100</div>
+            </div>
+          </div>
+        </div>
+        <div class="block_player">
+          <div class="raw_players">
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+          </div>
+          <div class="raw_players">
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+          </div>
+          <div class="raw_players">
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+            <div class="player_profil_red">
+              <div class="player_pic_red">
+                <div class="country"><img src="images/flag.png" loading="lazy" alt="" class="us"></div><img src="images/avatar_man.png" loading="lazy" alt="" class="player_def_pic">
+              </div>
+              <div class="player_amount">
+                <div class="dlape-copy">25 ETH</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="push_red"><img src="images/arrow_down.svg" loading="lazy" alt="" class="arrow_red"></div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=656751a4fe95d82133d70f59" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+</body>
+</html>` }} />
+    </>
+  );
+}
